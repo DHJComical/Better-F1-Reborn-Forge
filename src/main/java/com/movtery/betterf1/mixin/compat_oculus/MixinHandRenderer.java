1ef8995680feed163c1ab3024bf72646d8bb28ce
DHJComical <dhjcomical@163.com>
2026-09-04 12:32:14 +0800

    Port Better F1 mod logic from 26.1

---
+++ src/main/java/com/movtery/betterf1/mixin/compat_oculus/MixinHandRenderer.java
@@ -0,0 +1,26 @@
+package com.movtery.betterf1.mixin.compat_oculus;
+
+import com.movtery.betterf1.BetterF1;
+import com.movtery.betterf1.client.HUDState;
+import net.coderbot.iris.pipeline.HandRenderer;
+import net.minecraft.client.Options;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(HandRenderer.class)
+public class MixinHandRenderer {
+
+    @Redirect(
+            method = "canRender",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/Options;hideGui:Z"
+            )
+    )
+    private boolean canRender(Options instance) {
+        return instance.hideGui && BetterF1.state.equals(HUDState.ALL_HIDDEN);
+    }
+}
