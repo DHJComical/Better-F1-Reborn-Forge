5afbaaaa1b0e290fd4858b966648d83e27064947
DHJComical <dhjcomical@163.com>
2026-09-04 12:29:56 +0800

    Port Better F1 mod logic from 26.1

--- src/main/java/com/movtery/betterf1/BetterF1.java
+++ src/main/java/com/movtery/betterf1/BetterF1.java
@@ -1,9 +1,12 @@
 package com.movtery.betterf1;
 
+import com.movtery.betterf1.client.HUDState;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("betterf1")
 public class BetterF1 {
+    public static HUDState state = HUDState.ALL_VISIBLE;
+
     public BetterF1() {
     }
 }
